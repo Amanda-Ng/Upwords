252a86866f71713c4f55c001f87aa997665d8b54
Amanda-Ng <89530159+Amanda-Ng@users.noreply.github.com>
2024-03-31 04:11:05 +0000

    commit

--- include/hw3.h
+++ include/hw3.h
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 
-typedef struct GameState GameState;
+// typedef struct GameState GameState;
 
-typedef struct GameStateStackNode {
-    GameState *state;
-    struct GameStateStackNode *next;
-} GameStateStackNode;
+// typedef struct GameStateStackNode {
+//     GameState *state;
+//     struct GameStateStackNode *next;
+// } GameStateStackNode;
 
-typedef struct {
-    GameStateStackNode *top;
-} GameStateStack;
+// typedef struct {
+//     GameStateStackNode *top;
+// } GameStateStack;
 
 typedef struct GameState
 {
@@ -18,7 +18,7 @@ typedef struct GameState
     int **stack_heights;
     int rows;
     int cols;
-    GameStateStack *history;
+    // GameStateStack *history;
 } GameState;
 
 GameState* initialize_game_state(const char *filename);
